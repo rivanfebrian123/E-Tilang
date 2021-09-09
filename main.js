ad52cf7efc98def5909f10b28d7a75ab25480aa9
Muhammad Rivan <rivanfebrian123@gmail.com>
2021-09-10 06:24:39 +0700

    update

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function render(nama, kendaraan, noTilang, denda, pasal, bukti) {
     /tnp/gi, "Tanpa").replace(
     /plat/gi, "Plat")
 
-  return '<div class="item result"><div class="atas flex baseline">' +
+  return '<div class="item hasil"><div class="atas flex baseline">' +
     '  <h3>' + nama + '</h3>' +
     '  <h4>' + kendaraan_id + '</h4>' +
     '</div>' +
