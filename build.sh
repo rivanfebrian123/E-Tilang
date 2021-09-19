3273eb827e461e299f4a5c5493391ff19e3b65d9
Muhammad Rivan <rivanfebrian123@gmail.com>
2021-09-19 21:14:05 +0700

    update

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 set -e
 
-SCOUR_APP="scour --enable-viewboxing --enable-id-stripping --enable-comment-stripping --shorten-ids --remove-descriptive-elements --indent=none --strip-xml-space"
+SCOUR_APP="scour --enable-viewboxing --enable-id-stripping "
+SCOUR_APP+="--enable-comment-stripping --shorten-ids --remove-descriptive-elements "
+SCOUR_APP+="--indent=none --strip-xml-space --create-groups"
 
-# ${SCOUR_APP} -i data/logo.svg -o dist/logo.svg
-# ${SCOUR_APP} -i data/id.svg -o dist/id.svg
-# ${SCOUR_APP} -i data/cari.svg -o dist/cari.svg
-# ${SCOUR_APP} -i data/dompet.svg -o dist/dompet.svg
-# ${SCOUR_APP} -i data/palu.svg -o dist/palu.svg
-# ${SCOUR_APP} -i data/uang.svg -o dist/uang.svg
-sassc data/query.scss dist/query.css -t compressed
+${SCOUR_APP} -i data/logo.svg -o dist/logo.svg &
+${SCOUR_APP} -i data/id.svg -o dist/id.svg &
+${SCOUR_APP} -i data/cari.svg -o dist/cari.svg &
+${SCOUR_APP} -i data/dompet.svg -o dist/dompet.svg &
+${SCOUR_APP} -i data/palu.svg -o dist/palu.svg &
+${SCOUR_APP} -i data/uang.svg -o dist/uang.svg &
+sassc data/query.scss dist/query.css -t compressed &
 
-git add .
 python3 -m http.server
