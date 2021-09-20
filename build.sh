8d80578ebe3263c2c83c5635293af5d761c81d3f
Muhammad Rivan <rivanfebrian123@gmail.com>
2021-09-20 08:06:20 +0700

    update

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ SCOUR_APP="scour --enable-viewboxing --enable-id-stripping "
 SCOUR_APP+="--enable-comment-stripping --shorten-ids --remove-descriptive-elements "
 SCOUR_APP+="--indent=none --strip-xml-space --create-groups"
 
-${SCOUR_APP} -i src/logo.svg -o dist/logo.svg &
-${SCOUR_APP} -i src/id.svg -o dist/id.svg &
-${SCOUR_APP} -i src/cari.svg -o dist/cari.svg &
-${SCOUR_APP} -i src/dompet.svg -o dist/dompet.svg &
-${SCOUR_APP} -i src/palu.svg -o dist/palu.svg &
-${SCOUR_APP} -i src/uang.svg -o dist/uang.svg &
-sassc src/query.scss dist/query.css -t compressed &
+${SCOUR_APP} -i src/logo.svg -o dist/logo.svg
+${SCOUR_APP} -i src/id.svg -o dist/id.svg
+${SCOUR_APP} -i src/cari.svg -o dist/cari.svg
+${SCOUR_APP} -i src/dompet.svg -o dist/dompet.svg
+${SCOUR_APP} -i src/palu.svg -o dist/palu.svg
+${SCOUR_APP} -i src/uang.svg -o dist/uang.svg
+sassc src/query.scss dist/query.css -t compressed
 
 python3 -m http.server
