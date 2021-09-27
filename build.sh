1e9f0802e3473640fa33e095fbf97fdf310a3521
Muhammad Rivan <rivanfebrian123@gmail.com>
2021-09-27 08:20:29 +0700

    update

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-SCOUR_APP="scour --enable-viewboxing --enable-id-stripping "
-SCOUR_APP+="--enable-comment-stripping --shorten-ids --remove-descriptive-elements "
-SCOUR_APP+="--indent=none --strip-xml-space --create-groups"
+SCOUR="scour --enable-viewboxing --enable-id-stripping "
+SCOUR+="--enable-comment-stripping --shorten-ids --remove-descriptive-elements "
+SCOUR+="--indent=none --strip-xml-space --create-groups"
+SASSC="sassc -t compressed --omit-map-comment"
 
-# ${SCOUR_APP} -i src/img/logo.svg -o dist/logo.svg
-# ${SCOUR_APP} -i src/img/id.svg -o dist/id.svg
-# ${SCOUR_APP} -i src/img/cari.svg -o dist/cari.svg
-# ${SCOUR_APP} -i src/img/dompet.svg -o dist/dompet.svg
-# ${SCOUR_APP} -i src/img/palu.svg -o dist/palu.svg
-# ${SCOUR_APP} -i src/img/uang.svg -o dist/uang.svg
+# ${SCOUR} -i src/img/logo.svg -o dist/logo.svg
+# ${SCOUR} -i src/img/id.svg -o dist/id.svg
+# ${SCOUR} -i src/img/cari.svg -o dist/cari.svg
+# ${SCOUR} -i src/img/dompet.svg -o dist/dompet.svg
+# ${SCOUR} -i src/img/palu.svg -o dist/palu.svg
+# ${SCOUR} -i src/img/uang.svg -o dist/uang.svg
 
 # inkscape src/assets.svg -i app -o dist/ikon-app.png
 # inkscape src/assets.svg -i tab -o dist/ikon-tab.png
@@ -18,10 +19,10 @@ SCOUR_APP+="--indent=none --strip-xml-space --create-groups"
 # optipng -o7 dist/ikon-tab.png
 
 # inkscape src/assets.svg -i pin -o dist/ikon-pin-ori.svg
-# ${SCOUR_APP} -i dist/ikon-pin-ori.svg -o dist/ikon-pin.svg
+# ${SCOUR} -i dist/ikon-pin-ori.svg -o dist/ikon-pin.svg
 # rm -rf dist/ikon-pin-ori.svg
 
 # slimit -m -t src/main.js > dist/main.js
-sassc src/query.scss dist/query.css -t compressed
+${SASSC} src/query.scss dist/query.css
 
 python3 -m http.server
